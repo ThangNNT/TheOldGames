b5595ad4fa4c03b44c361b75909f16c37605d6a1
thangnguyen <thangnguyen@okxe.vn>
2022-06-08 16:42:14 +0700

    handle move logic

--- app/src/main/java/com/nnt/theoldgames/screen/huntingsnake/playscreen/SnakePlayScreen.kt
+++ app/src/main/java/com/nnt/theoldgames/screen/huntingsnake/playscreen/SnakePlayScreen.kt
@@ -5,25 +5,54 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.nnt.theoldgames.screen.huntingsnake.playscreen.SnakePlayViewModel
 
+@ExperimentalUnitApi
 @ExperimentalFoundationApi
 @Composable
 fun SnakePlayScreen(navController: NavController = rememberNavController()){
     val viewModel = hiltViewModel<SnakePlayViewModel>()
     val screenWithDp = LocalContext.current.resources.configuration.screenWidthDp
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        SnakePlayGround(viewModel, screenWithDp, rememberLazyListState())
+    val score = viewModel.score.value
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Row(horizontalArrangement = Arrangement.End) {
+                Text("Score: ")
+                Text(text = "$score")
+            }
+            SnakePlayGround(viewModel, screenWithDp)
+            Spacer(modifier = Modifier.height(24.dp))
+            Controller {
+                viewModel.move(it)
+            }
+        }
+    }
+    viewModel.isDead.value.apply {
+        if(this){
+            DeadDialog(onReplay = {viewModel.reset()}, onExit = {
+                viewModel.isDead.value = false
+                navController.popBackStack() })
+        }
     }
 }
 
@@ -32,27 +61,102 @@ fun SnakePlayScreen(navController: NavController = rememberNavController()){
 fun SnakePlayGround(
     viewModel: SnakePlayViewModel,
     screenWidthDp: Int,
-    listState: LazyListState,
     margin: Int = 16
 ) {
     val boxPerRow = viewModel.boxPerRow
-    val snake = viewModel.snakeMaps.collectAsState().value
-    val boxSize = ((screenWidthDp - margin * 2) / boxPerRow).dp
-    LazyVerticalGrid(
-        cells = GridCells.Fixed(boxPerRow),
-        state = listState,
-        modifier = Modifier
-            .padding(margin.dp)
-            .border(width = 2.dp, Color.Black)
+    val snake = viewModel.snakeCoordinate
+    val boxSize = ((screenWidthDp - margin * 2) / boxPerRow)
+    val foodPos = viewModel.foodPositionState.collectAsState().value
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        items(boxPerRow*boxPerRow){ index ->
-            if(snake.contains(index)){
-                Box(modifier = Modifier.size(boxSize).background(Color.Black))
+        BoxWithConstraints(
+            modifier = Modifier
+                .padding(top = 16.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .size((boxPerRow * boxSize).dp)
+                    .border(2.dp, Color.Blue)
+            )
+            Box(
+                modifier = Modifier
+                    .size(boxSize.dp)
+                    .offset(x = (foodPos.first * boxSize).dp, y = (foodPos.second * boxSize).dp)
+                    .background(Color.Blue, shape = CircleShape)
+            )
+            snake.forEach {
+                Box(
+                    modifier = Modifier
+                        .size(boxSize.dp)
+                        .offset(x = (it.first * boxSize).dp, y = (it.second * boxSize).dp)
+                        .background(Color.Blue, shape = RoundedCornerShape(2.dp))
+                ) {
+                }
             }
-            else {
-                Box(modifier = Modifier.size(boxSize))
-            }
+        }
+    }
+}
 
+@ExperimentalFoundationApi
+@Preview
+@Composable
+fun Controller(onMove: (direction: SnakePlayViewModel.RunDirection)-> Unit = {}){
+    val buttonWidth = 60.dp
+    LazyVerticalGrid(cells = GridCells.Fixed(3), modifier = Modifier.width(180.dp)){
+        items(9){ index ->
+            when(index){
+                1 -> {
+                    Button(onClick = {onMove.invoke(SnakePlayViewModel.RunDirection.Up)}) {
+                        Text(text = "^", modifier = Modifier.width(buttonWidth), textAlign = TextAlign.Center)
+                    }
+                }
+                3 -> {
+                    Button(onClick = {onMove.invoke(SnakePlayViewModel.RunDirection.Left)}) {
+                        Text(text = "<", modifier = Modifier.width(buttonWidth), textAlign = TextAlign.Center)
+                    }
+                }
+                5 -> {
+                    Button(onClick = {onMove.invoke(SnakePlayViewModel.RunDirection.Right)}) {
+                        Text(text = ">", modifier = Modifier.width(buttonWidth), textAlign = TextAlign.Center)
+                    }
+                }
+                7 -> {
+                    Button(onClick = {onMove.invoke(SnakePlayViewModel.RunDirection.Down)}) {
+                        Text(text = "v", modifier = Modifier.width(buttonWidth), textAlign = TextAlign.Center)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@ExperimentalUnitApi
+@Composable
+fun DeadDialog(onReplay: ()-> Unit, onExit: ()-> Unit){
+    val buttonWith= 80.dp
+    Dialog(onDismissRequest = { onReplay.invoke()}){
+        Column(
+            modifier = Modifier
+                .padding(bottom = 100.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Game over", color = Color.White, fontSize = TextUnit(20f, TextUnitType.Sp))
+            Spacer(modifier = Modifier.height(100.dp))
+            Row {
+                Button(onClick = { onReplay.invoke() }) {
+                    Text(text = "Replay", modifier = Modifier.width(buttonWith), textAlign = TextAlign.Center)
+                }
+                Spacer(modifier = Modifier.width(20.dp))
+                Button(onClick = {
+                    onExit.invoke() }) {
+                    Text(text = "Exit", modifier = Modifier.width(buttonWith), textAlign = TextAlign.Center)
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/nnt/theoldgames/screen/huntingsnake/playscreen/SnakePlayViewModel.kt
+++ app/src/main/java/com/nnt/theoldgames/screen/huntingsnake/playscreen/SnakePlayViewModel.kt
@@ -1,103 +1,261 @@
 package com.nnt.theoldgames.screen.huntingsnake.playscreen
 
-import android.util.Log
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import java.util.*
 import javax.inject.Inject
-import kotlin.collections.ArrayList
-import kotlin.collections.LinkedHashMap
-import kotlin.collections.LinkedHashSet
+import kotlin.random.Random
 
 @HiltViewModel
 class SnakePlayViewModel @Inject constructor() : ViewModel(){
-    //Matrix [50,50]
-    private val snakePositions = LinkedList<Pair<Int, Int>>()
-    private val _snakeState = MutableStateFlow<LinkedList<Pair<Int, Int>>>(LinkedList())
-    val snakeState: StateFlow<LinkedList<Pair<Int, Int>>> = _snakeState
+    val snakeCoordinate = mutableStateListOf<Pair<Int, Int>>()
 
-    private val _foodPosition = MutableStateFlow(Pair(0,0))
-    val foodPosition: StateFlow<Pair<Int, Int>> = _foodPosition
-
-    private val snakeMap = LinkedHashSet<Int>()
-    private val _snakeMaps = MutableStateFlow(snakeMap)
-    val snakeMaps: StateFlow<LinkedHashSet<Int>> = _snakeMaps
+    private val _foodPositionState = MutableStateFlow(Pair(0,0))
+    val foodPositionState: StateFlow<Pair<Int, Int>> = _foodPositionState
 
     val boxPerRow = 50
 
+    private val _score = mutableStateOf(0)
+    val score: State<Int> = _score
+
+    private val snakeSet = mutableSetOf<Int>()
+
+    private var snakeRunJob: Job? = null
+
+    val isDead = mutableStateOf(false)
+    private var initSnakeLength = 2
+
     init {
+        reset()
+    }
+
+    fun reset(){
+        snakeRunJob?.cancel()
+        snakeCoordinate.clear()
+        isDead.value = false
         val head = Pair(25, 25)
         val neck = Pair (24, 25)
         addSnake(pair = head)
         addSnake(pair = neck)
-        _snakeState.value = snakePositions
-        _snakeMaps.value = snakeMap
-        viewModelScope.launch {
+        for(i in 23 downTo 3){
+            addSnake(pair = Pair(i, 25))
+        }
+        initSnakeLength = snakeCoordinate.size
+        snakeRunJob = viewModelScope.launch(Dispatchers.Default) {
+            delay(1000)
             while (true) {
-                delay(1000)
+                delay(100)
                 runSnake()
             }
         }
+        randomFood()
     }
+
+    private fun getRandomPosition(): Pair<Int, Int>{
+        val x = Random.nextInt(0, boxPerRow - 1)
+        val y = Random.nextInt(0, boxPerRow - 1)
+        return Pair(x, y)
+    }
+
+    private fun randomFood(){
+        val foodPosition = getRandomPosition()
+        if(isValidFoodPosition(foodPos = foodPosition)){
+            _foodPositionState.value = foodPosition
+        }
+        else randomFood()
+    }
+
+    private fun isValidFoodPosition(foodPos: Pair<Int, Int>): Boolean {
+        snakeCoordinate.forEachIndexed { _, coordinate ->
+            if(foodPos.isDuplicate(coordinate)){
+                return false
+            }
+        }
+        return true
+    }
+
     private fun addSnake(index: Int = -1, pair: Pair<Int, Int>){
         if(index==-1){
-            snakePositions.add(pair)
+            snakeCoordinate.add(pair)
         }
         else {
-            snakePositions.add(index, pair)
+            snakeCoordinate.add(index, pair)
         }
-        snakeMap.add(pair.convertToIndex())
+        snakeSet.add(pair.convertToIndex())
+
     }
 
     private fun removeSnakeTail() {
-        snakePositions.last.convertToIndex().let {
-            snakeMap.remove(it)
-        }
-        snakePositions.removeLast()
+        snakeSet.add(snakeCoordinate.last().convertToIndex())
+        snakeCoordinate.removeLast()
     }
 
     private fun runSnake(){
-        val snakeHead = snakePositions[0]
-        val snakeNeck = snakePositions[1]
-        when(getDirection(snakeHead, snakeNeck)){
+        move()
+    }
+
+    /**
+     * if direction null, the snake will move forward
+     */
+    fun move(direction: RunDirection? = null){
+        val snakeHead = snakeCoordinate[0]
+        val snakeNeck = snakeCoordinate[1]
+        val currentDirection = getDirection(snakeHead, snakeNeck)
+        if ((currentDirection == RunDirection.Down && direction == RunDirection.Up)
+            || (currentDirection == RunDirection.Up && direction == RunDirection.Down)
+            || (currentDirection == RunDirection.Left && direction == RunDirection.Right)
+            || (currentDirection == RunDirection.Right && direction == RunDirection.Left)
+        ) return
+        when(direction ?: currentDirection){
             RunDirection.Up -> {
-                addSnake(0, Pair(snakeHead.first, snakeHead.second-1))
+                val newHead = if(isReachTop(snakeHead, snakeNeck)){
+                    Pair(snakeHead.first, boxPerRow-1)
+                }
+                else {
+                    Pair(snakeHead.first, snakeHead.second-1)
+                }
+                if(isDead(newHead)){
+                    die()
+                    return
+                }
+                addSnake(0, newHead)
                 removeSnakeTail()
             }
             RunDirection.Down -> {
-                addSnake(0, Pair(snakeHead.first, snakeHead.second+1))
+                val newHead = if(isReachBottom(snakeHead, snakeNeck)){
+                    Pair(snakeHead.first, 0)
+                }
+                else {
+                    Pair(snakeHead.first, snakeHead.second+1)
+                }
+                if(isDead(newHead)){
+                    die()
+                    return
+                }
+                addSnake(0, newHead)
                 removeSnakeTail()
             }
             RunDirection.Left -> {
-                addSnake(0, Pair(snakeHead.first-1, snakeHead.second))
+                val newHead = if (isReachLeft(snakeHead, snakeNeck)) {
+                    Pair(boxPerRow - 1, snakeHead.second)
+                } else {
+                    Pair(snakeHead.first - 1, snakeHead.second)
+                }
+                if(isDead(newHead)){
+                    die()
+                    return
+                }
+                addSnake(0, newHead)
                 removeSnakeTail()
             }
             RunDirection.Right -> {
-                addSnake(0, Pair(snakeHead.first+1, snakeHead.second))
+                val newHead = if(isReachRight(snakeHead, snakeNeck)){
+                    Pair(0, snakeHead.second)
+                }
+                else {
+                    Pair(snakeHead.first+1, snakeHead.second)
+                }
+                if(isDead(newHead)){
+                    die()
+                    return
+                }
+                addSnake(0, newHead)
                 removeSnakeTail()
             }
         }
-        _snakeMaps.value = snakeMap
-        _snakeState.value = snakePositions
+        val newHead = snakeCoordinate[0]
+        if(newHead.isDuplicate(foodPositionState.value)){
+            ateFood()
+        }
     }
 
-    private fun getDirection(head: Pair<Int,Int>, neck: Pair<Int, Int>): RunDirection{
+    private fun die(){
+        isDead.value = true
+        snakeRunJob?.cancel()
+    }
+
+    private fun ateFood(){
+        randomFood()
+        val snakeTail1 = snakeCoordinate.last()
+        val snakeTail2 = snakeCoordinate[snakeCoordinate.size-2]
+        when(getDirection(snakeTail1, snakeTail2)){
+            RunDirection.Left -> {
+                if(isReachRight(snakeTail1, snakeTail2)){
+                    addSnake(pair = Pair(0, snakeTail1.second))
+                }
+                else {
+                    addSnake(pair = Pair(snakeTail1.first+1, snakeTail1.second))
+                }
+            }
+            RunDirection.Up -> {
+                if(isReachBottom(snakeTail1, snakeTail2)){
+                    addSnake(pair = Pair(snakeTail1.first, 0))
+                }
+                else addSnake(pair = Pair(snakeTail1.first, snakeTail1.second+1))
+            }
+            RunDirection.Right -> {
+                if(isReachLeft(snakeTail1, snakeTail2)){
+                    addSnake(pair = Pair(boxPerRow-1, snakeTail1.second))
+                }
+                addSnake(pair = Pair(snakeTail1.first-1, snakeTail1.second))
+            }
+            RunDirection.Down -> {
+                if (isReachTop(snakeTail1, snakeTail2)) {
+                    addSnake(pair = Pair(snakeTail1.first, boxPerRow-1))
+
+                } else addSnake(pair = Pair(snakeTail1.first, snakeTail1.second-1))
+            }
+        }
+        _score.value = (snakeCoordinate.size - initSnakeLength)*10
+    }
+
+    fun Pair<Int, Int>.isDuplicate(pair: Pair<Int, Int>): Boolean{
+        return this.first == pair.first && this.second == pair.second
+    }
+
+    private fun isReachTop(head: Pair<Int, Int>, neck: Pair<Int, Int>): Boolean {
+        return head.second == 0
+    }
+    private fun isReachBottom(head: Pair<Int, Int>, neck: Pair<Int, Int>): Boolean {
+        return head.second == boxPerRow-1
+    }
+    private fun isReachLeft(head: Pair<Int, Int>, neck: Pair<Int, Int>): Boolean {
+        return head.first == 0
+    }
+    private fun isReachRight(head: Pair<Int, Int>, neck: Pair<Int, Int>): Boolean {
+        return head.first == boxPerRow-1
+    }
+
+    private fun isDead(head: Pair<Int, Int>): Boolean{
+        snakeCoordinate.forEach {
+            if (head.isDuplicate(it)){
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun getDirection(head: Pair<Int,Int>, neck: Pair<Int, Int>): RunDirection {
         when {
-            head.first == neck.first && head.second+1==neck.second ->{
+            head.first == neck.first && (head.second+1==neck.second || (head.second == boxPerRow-1 && neck.second == 0)) ->{
                 return RunDirection.Up
             }
-            head.first == neck.first && head.second-1==neck.second -> {
+            head.first == neck.first && (head.second-1==neck.second || (head.second == 0 && neck.second == boxPerRow-1)) -> {
                 return RunDirection.Down
             }
-            head.second == neck.second && head.first+1==neck.first ->{
+            head.second == neck.second && (head.first+1==neck.first || (head.first == boxPerRow-1 && neck.first == 0)) ->{
                 return RunDirection.Left
             }
-            head.first == neck.first && head.first-1==neck.first -> {
+            head.first == neck.first && (head.first-1==neck.first || (head.first == 0 && neck.first == boxPerRow-1)) -> {
                 return RunDirection.Right
             }
             else ->{
@@ -118,6 +276,6 @@ class SnakePlayViewModel @Inject constructor() : ViewModel(){
     }
 
     fun Pair<Int, Int>.convertToIndex(): Int {
-        return this.first+ this.second*boxPerRow+1
+        return this.first+ this.second*boxPerRow
     }
 }
